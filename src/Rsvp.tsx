7f61f30ecb652206b03412801a4d785505af3687
Jens Tschirpig <jens1101@users.noreply.github.com>
2022-11-04 13:56:48 +0200

    feat: Update RSVP page
    Used a different approach to display the form. It requires a more manual approach, but significantly improves the UX.

--- src/Rsvp.tsx
+++ src/Rsvp.tsx
@@ -1,37 +1,14 @@
-import { useState } from "react";
-
 export function Rsvp() {
-  const [showSpinner, setShowSpinner] = useState(true);
-
   return (
-    <div className={"d-flex flex-column flex-grow-1 position-relative"}>
-      <iframe
-        className={"flex-grow-1"}
-        title={"Jens and Simone Wedding RSVP"}
-        src={
-          "https://docs.google.com/forms/d/e/1FAIpQLSeeZx6yGJaceTdkq6wEr_a3znbGtvPCDeXwgbPMy-Cx8btFlA/viewform?embedded=true"
-        }
-        onLoad={() => setShowSpinner(false)}
-      />
-
-      <div
-        className={`${
-          showSpinner ? "d-flex" : "d-none"
-        } justify-content-center align-items-center position-absolute top-0 start-0 end-0 bottom-0`}
-      >
-        <div
-          className={"spinner-grow text-primary"}
-          role={"status"}
-          style={{
-            width: "10vmin",
-            height: "10vmin",
-            minHeight: "3rem",
-            minWidth: "3rem",
-          }}
-        >
-          <span className={"visually-hidden"}>Loading...</span>
-        </div>
-      </div>
-    </div>
+    <iframe
+      className={"flex-grow-1"}
+      style={{
+        height: "1600px",
+      }}
+      title={"Jens and Simone Wedding RSVP"}
+      src={
+        "https://docs.google.com/forms/d/e/1FAIpQLSeeZx6yGJaceTdkq6wEr_a3znbGtvPCDeXwgbPMy-Cx8btFlA/viewform?embedded=true"
+      }
+    />
   );
 }
